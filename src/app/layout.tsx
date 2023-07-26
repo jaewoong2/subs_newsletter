35a47abd6cf518add949e1c005f0ca40b1ca3d20
jaewoong2 <jwisgenius@naver.com>
2023-07-26 21:49:39 +0900

    :lipstick: 폰트 추가를 위한 <link /> 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,11 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang='kr' className='h-full overflow-scroll'>
+      <head>
+        <link rel='preconnect' href='https://cdn.jsdelivr.net' />
+        <link rel='preconnect' href='https://cdn.jsdelivr.net' crossOrigin='' />
+        <link href='https://cdn.jsdelivr.net/gh/toss/tossface/dist/tossface.css' rel='stylesheet' type='text/css' />
+      </head>
       <body suppressHydrationWarning={true} className='relative max-h-full overflow-scroll bg-white'>
         <ThemeProviders>
           <Providers>{children}</Providers>
