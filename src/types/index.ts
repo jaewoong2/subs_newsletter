00b5cca5bcfff192ce1604172cbcccaf220b80e6
jaewoong2 <jwisgenius@naver.com>
2023-08-27 14:03:37 +0900

    :label: update type article

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,7 @@ import { Database } from './supabase'
 export type NewsLetter = Database['public']['Tables']['newsletter']['Row']
 export type Checksome = Database['public']['Tables']['checksome']['Row']
 export type Users = Database['public']['Tables']['users']['Row']
+export type Articles = Database['public']['Tables']['article']['Row']
 
 export type NextPageProps<
   Params extends Record<string, string> | null = null,
