99fc3cc18ab4f17861cfa308f5927d36dd58b98d
50461 <lime@sk.com>
2023-09-28 16:43:32 +0900

    :fire: null assertion '!'

--- src/app/server-sitemap.xml/route.ts
+++ src/app/server-sitemap.xml/route.ts
@@ -2,7 +2,10 @@ import { getServerSideSitemap } from 'next-sitemap'
 import { createClient } from '@supabase/supabase-js'
 
 export async function GET() {
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
+  )
 
   const newsletters = await supabase.from('newsletter').select('name')
   const aritcles = await supabase.from('article').select('title')
